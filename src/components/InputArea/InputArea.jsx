908791a4dbda385ecc17e9bf0e0f53a47aae5cf6
joaootaviofm <joaootaviofm@gmail.com>
2025-02-02 14:12:54 -0300

    add useState

--- src/components/InputArea/InputArea.jsx
+++ src/components/InputArea/InputArea.jsx
@@ -1,10 +1,25 @@
 import './InputArea.css'
-
+import { useState } from 'react';
 function InputArea(){
+
+    const [count,setCount] = useState(0)
+
+    function onIncreaseHandler(){
+        setCount(count+1)
+    }
+    
+    function onDecreaseHandler(){
+        setCount(count-1)
+    }
+
     return (
         <div>
             <div className="container">
-                a
+                <div className="inputDiv">
+                    <button onClick={onIncreaseHandler} className='operatorButton'>+</button>
+                    <h1>{count}</h1>
+                    <button onClick={onDecreaseHandler} className='operatorButton'>-</button>
+                </div>
             </div>
         </div>
     )
